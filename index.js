f1ed30f43cd6e059c714a4333644d6db243164a0
hanhan9449 <haannn@qq.com>
2020-03-26 02:06:42 +0800

    heroku 443port

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const http = require('http')
 const server = http.createServer((req, res) => {
 	res.writeHead(200, { "Content-Type": "text/plain" })
 	res.end("Heroku")
-)
-server.listen(3000)
+})
+server.listen(443)
 
